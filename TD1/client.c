f98204a6d9ecdb0e838d9dd490c6dd2ef8eea18d
Titouan-Joseph <titouanjoseph@gmail.com>
2020-09-28 11:54:56 +0200

    TD1

--- TD1/client.c
+++ TD1/client.c
@@ -1,31 +1,56 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <string.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
 int main() {
     printf("Starting Client \n");
     int sock = 0;
-    //struct sockaddr_in serv_addr;
-    //memset((char*)&my_addr, 0, sizeof(serv_addr)) ;
+    int PORT = 1234;
+    struct sockaddr_in serv_addr;
+    memset((char*)&serv_addr, 0, sizeof(serv_addr)) ;
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    printf(" \n Valeur du descripteur client : %d \n", sock )
+    printf(" \n Valeur du descripteur client : %d \n", sock );
 
-    //serv_addr.sin_family = AF_INET;
-    //serv_addr.sin_port = htons(PORT);
-    //if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    //{
-    //    printf("\n Socket creation error for client \n");
-    //    int reuse= 1 ;
-    //    setsockopt(descripteur, SOL_SOCKET, SO_REUSEADDR,&reuse, sizeof(reuse)) ;
-    //    return -1;
-    //}
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    if (sock < 0)
+    {
+        printf("\n Socket creation error for client \n");
+        int reuse= 1 ;
+        setsockopt(sock, SOL_SOCKET, SO_REUSEADDR,&reuse, sizeof(reuse)) ;
+        return -1;
+    }
 
-    //char loc = "127.0.0.1"
-    //int adr = inet_aton(AF_INET, loc, &serv_addr.sin_addr);
-    //printf("\n Valeur retournée par la fonction inet_aton() : %c", adr);
+    // char *loc = "127.0.0.1";
+    // struct sockaddr_in addr;
+    // int val = inet_aton(loc, &addr.sin_addr);
+    // char *some_addr = inet_ntoa(addr.sin_addr);
+    // printf("\n Valeur retournée par la fonction inet_aton() : %s \n", some_addr);
+    // printf("\n Valeur retournée par la fonction inet_aton() : %d \n", val);
+
+    // int conv = htons((long) &addr);
+    // printf("val conv : %d \n", conv);
+
+    int conn = connect(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+    printf("connect value : %d \n", conn);
+    char *caracter;
+    char *received;
+    caracter=" Hello from client ";
+    int res = write(conn, caracter, strlen(caracter));
+    //printf("Message sent to server %d\n", res);
+    //int res = read(sock, received, 1024);
+    printf("Value %d", res);
+    printf("Received value from server : %s\n",received );
+
+
+    // printf("%d\n", adr.s_addr);
+
 
 
     return 0;
--- TD1/server.c
+++ TD1/server.c
@@ -4,39 +4,55 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
 int main() {
     printf("Starting Server \n");
     int server_fd, new_socket, valread;
-    server_fd = socket(AF_INET, SOCK_STREAM, 0)
-    printf(" \n Valeur du descripteur serveur : %d \n", server_fd )
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    printf(" \n Valeur du descripteur serveur : %d \n", server_fd );
 
 
-    //struct sockaddr_in address;
-    //memset((char*)&my_addr, 0, sizeof(address)) ;
+    struct sockaddr_in address;
+    int PORT = 1234;
+    memset((char*)&address, 0, sizeof(address)) ;
     //server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    //address.sin_family = AF_INET;
-    //address.sin_addr.s_addr = INADDR_ANY;
-    //address.sin_port = htons( PORT );
-    //server_fd = socket(AF_INET, SOCK_STREAM, 0))
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
 
-    //if ((server_fd  < 0)
-    //{
-    //    printf("\n Socket creation error for server \n");
-    //    int reuse= 1 ;
-    //    setsockopt(descripteur, SOL_SOCKET, SO_REUSEADDR,&reuse, sizeof(reuse)) ;
-    //    return -1;
-    //}
+    printf(" valeur de INADDR_ANY : %d  \n", INADDR_ANY);
+    printf(" valeur de LOOPBACK : %d \n", INADDR_LOOPBACK);
 
-    //int connect = bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-    //int max_co = 3;
-    //int is_listening = listen(server_fd, max_co);
+    int reuse= 1 ;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR,&reuse, sizeof(reuse)) ;
+    if (server_fd  < 0) {
+        printf("\n Socket creation error for server \n");
+        return -1;
+    }
+
+    int connect = bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    int max_co = 3;
+    int is_listening = listen(server_fd, max_co);
 
     //----------------------------
     //boucles infinies :
+    //
+    struct sockaddr_in addr;
+    socklen_t var = sizeof(addr);
 
-    //for(;;)
-    //        ;
+    for(;;){
+      int ack = accept(server_fd, (struct sockaddr *) &address, &var);
+      printf("Accepted %d\n", ack );
+      char *caracter ;
+      char *received;
+      caracter = "Hello from server ";
+      //int res = write(ack, caracter, strlen(caracter));
+      //printf("Message sent to client \n");
+      int res = read(ack, received, 1024);
+      printf("Value %d", res);
+      //printf("Received value from client : %s\n",received );
+    }
 
     //ou
 
