20b2ba6f82bb783b0d09ade3ef808f1dae843bb6
Titouan-Joseph REVOL <titouanjoseph@gmail.com>
2020-11-27 12:51:05 +0100

    :rocket: on reinvente la fibre

--- Project/TCPoverUDP/common.py
+++ Project/TCPoverUDP/common.py
@@ -1,16 +1,21 @@
-UDP_IP = "127.0.0.1"
+UDP_IP = "0.0.0.0"
 UDP_PORT_CONTROL = 5005
 UDP_PORT_DATA = 6666
 BUFFER = 1024
 
-ACK = b"ack"
-SYN = b"syn"
-SYNACK = b"synack"
+ACK = b"ACK"
+SYN = b"SYN"
+SYNACK = b"SYN-ACK"
+
+FIN = b"FIN"
 
 bonjour = b"hello"
 aurevoir = b"Coucou toi !"
 sendimg = b"getimg"
 
-jpg = "jpg"
-pdf = "pdf"
-png = "png"
+
+jpg = ".jpg"
+pdf = ".pdf"
+png = ".png"
+mp3 = ".mp3"
+txt = ".txt"
