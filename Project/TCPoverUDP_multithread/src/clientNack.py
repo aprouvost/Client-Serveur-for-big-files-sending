0ab35cfe4490725fd399eee1eedbdec97088e62c
Titouan-Joseph REVOL <titouanjoseph@gmail.com>
2020-12-07 09:39:57 +0100

    :hankey: 0.7Mo/s sur les gros fichies

--- Project/TCPoverUDP_multithread/src/clientNack.py
+++ Project/TCPoverUDP_multithread/src/clientNack.py
@@ -16,11 +16,11 @@ def clientN_ack(sock, queue, lock):
     while True:
         try:
             data, _ = utils.recvFromClient(sock)
-            print(data)
+            # print(data)
             if re.match(r"ACK([0-9]){6}", data):
                 current_sequence_ack = int(data[3:])
                 last_sequence_ack = current_sequence_ack if current_sequence_ack > last_sequence_ack else last_sequence_ack
-                print(f"Received ACK : {last_sequence_ack}")
+                # print(f"Received ACK : {last_sequence_ack}")
         except socket.timeout:
             # envoyer au thread send la seq ack via une queue
             if last_sequence_ack != value_in_queue:
--- Project/TCPoverUDP_multithread/src/clientNsender.py
+++ Project/TCPoverUDP_multithread/src/clientNsender.py
@@ -4,12 +4,17 @@ import time
 import _queue
 import threading
 from clientNack import clientN_ack
+import socket
 
 
 def clientN_sender(sock, queue, lock):
     utils.init()
     print("sender thread")
-    rcv_filename, addr = utils.recvFromClient(sock)
+    try:
+        rcv_filename, addr = utils.recvFromClient(sock)
+    except socket.timeout:
+        print("No file demanding")
+        exit(0)
     current_path = os.getcwd()
     filename = current_path + "\\..\\..\\input\\" + rcv_filename  # TODO : changer pour que ca soit que la current dir pour le rendu
     try:
@@ -21,44 +26,40 @@ def clientN_sender(sock, queue, lock):
     except FileNotFoundError:
         print(" Hold on ! The file can't be found !")
         # TODO : Est ce qu'on envoie au client qu'on a pas trouver le fichier avec un FIN ?
+        utils.sendToClient(sock, addr, utils.FIN)
 
     # Envoi de la data au client
-    sequence = 1  # on commence a 1 sinon ca fait de la merde avec le client
-    time_start = time.perf_counter()
-    file_send = False
     length_buf = len(buf)
-    # clientNack = threading.Thread(target=clientN_ack, args=(sock, queue, lock))
-    # clientNack.start()
-    while not file_send:
+    c_window = 10
+    last_ack = -1
+    sequence = 1  # on commence a 1 sinon ca fait de la merde avec le client
+    RTT = 0.06
+    time_start = time.perf_counter()
+    # on envoie selon la congestion window
+    while True:
+        buf_window = buf[(sequence - 1):(sequence - 1) + c_window]
+        for data in buf_window:
+            sequence = buf.index(data) + 1
+            sequence = str(sequence).zfill(6).encode('utf-8')
+            sock.sendto(sequence + data, addr)
 
+        # on regarde les ack
         try:
-            last_ack = queue.get(block=False)
-            print(last_ack)
+            last_ack = queue.get(block=True, timeout=RTT)
             if last_ack == length_buf:
-                print(" Taille atteinte")
-                file_send = True
+                print("Taille atteinte")
+                break
             else:
-                for elem in range(0, last_ack):
-                    buf[elem] = 0
-        except _queue.Empty:
-            for data in buf:
-                if data == 0:
-                    continue
-                else:
-                    sequence = str(sequence).zfill(6).encode('utf-8')
-                    sock.sendto(sequence + data, addr)
-                    sequence = buf.index(data) + 1
+                sequence = last_ack
+                c_window += 1
+        except:
+            print("Congestion detected")
+            sequence = last_ack
+            c_window = int(c_window / 2)
 
-        # clientNack.join()
-        # lock.acquire()
-            # if not clientNack.is_alive():
-            #     clientNack.start()
-        # lock.release()
-
+    time_end = time.perf_counter()
     print(" File sent with success ")
 
-    # TODO : interuption en fonction de la sequence ack (avec une queue je pense)
-    time_end = time.perf_counter()
     time_run = time_end - time_start
     debit = file_size / time_run
     time.sleep(0.1)
